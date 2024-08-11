5b897383c3a125c9f248501d0a816f6148fc8945
naysamaria98 <132231465+Naysamaria98@users.noreply.github.com>
2024-08-11 04:45:57 +0000

    Aug 11, 2024, 4:45 PM

--- comp.py
+++ comp.py
@@ -317,51 +317,54 @@ def computer_turn():
     else:
         top_color = top_value = None
 
-    matching_card_found = False
-    special_card_played = False
+    # Flag to indicate if a card was played
+    card_played = False
 
     # Try to find a matching card in the computer's hand
     for card in computer_cards:
         card_color, card_value = card.split("_")
-        if (
-            card_color == top_color
-            or card_value == top_value
-            or card_value in wild_cards
-        ):
-            matching_card_found = True
+
+        # Check if it's a special card
+        if card_value in ["+2", "+4"]:
+            # Play the special card
+            computer_cards.remove(card)
+            discard_pile.insert(0, card)
+            print(f"Computer played card: {card}")
+
+            # Show message if the played card is a +2 or +4
+            if card_value == "+2":
+                show_message("Draw 2 cards", duration=3000)
+            elif card_value == "+4":
+                show_message("Draw 4 cards", duration=3000)
+
+            # Add cards to the player's hand
+            num_cards_to_draw = 2 if card_value == "+2" else 4
+            for _ in range(num_cards_to_draw):
+                if deck:
+                    drawn_card = deck.pop()
+                    player_cards.append(drawn_card)
+                    print(f"Player drew a card from the deck: {drawn_card}")
+
+            card_played = True
             break
 
-    if matching_card_found:
-        # Try to play a special card first if available
+    # If no special card was played, play a regular matching card
+    if not card_played:
         for card in computer_cards:
             card_color, card_value = card.split("_")
-            if card_value in ["+2", "+4"]:
-                # Play the special card
+            if (
+                card_color == top_color
+                or card_value == top_value
+                or card_value in wild_cards
+            ):
                 computer_cards.remove(card)
                 discard_pile.insert(0, card)
                 print(f"Computer played card: {card}")
-
-                # Show message if the played card is a +2 or +4
-                if card_value in ["+2", "+4"]:
-                    show_message("Draw cards", duration=3000)
-                    special_card_played = True
+                card_played = True
                 break
 
-        # If no special card was played, play a regular matching card
-        if not special_card_played:
-            for card in computer_cards:
-                card_color, card_value = card.split("_")
-                if (
-                    card_color == top_color
-                    or card_value == top_value
-                    or card_value in wild_cards
-                ):
-                    computer_cards.remove(card)
-                    discard_pile.insert(0, card)
-                    print(f"Computer played card: {card}")
-                    break
-    else:
-        # Draw a card from the deck if no matching card is found
+    # If no card was played, draw a card from the deck
+    if not card_played:
         if deck:
             drawn_card = deck.pop()
             computer_cards.append(drawn_card)
@@ -382,6 +385,7 @@ def show_message(message, duration=1500):
     pygame.display.flip()
     pygame.time.wait(duration)
     screen.fill((0, 0, 0))  # Clear the screen after displaying the message
+
 
 
 def finish_game(message):
